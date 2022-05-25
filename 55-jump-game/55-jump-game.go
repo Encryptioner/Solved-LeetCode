0a0fa87ad22f60295aea0ba4e85f44db1aa3eadf
Mir Mursalin Ankur <mir.ankur.ruet13@gmail.com>
2022-05-26 00:36:44 +0600

    Time: 116 ms (39.64%), Space: 7.3 MB (66.41%) - LeetHub

---
+++ 55-jump-game/55-jump-game.go
@@ -0,0 +1,54 @@
+/*
+ * Ankur Mursalin
+ *
+ * https://encryptioner.github.io/
+ *
+ * Created on Wed May 25 2022
+ */
+
+// REFERENCE: https://leetcode.com/problems/jump-game/
+
+// package main
+
+// import "fmt"
+
+func canJump(nums []int) bool {
+	if len(nums) <= 1 {
+		return true
+	}
+	lastIndex := len(nums) - 1
+	nearestGoodIndex := lastIndex
+
+	for i := lastIndex; i > 0; i-- {
+		checkingIndex := nums[i] + i
+		if checkingIndex >= nearestGoodIndex {
+			nearestGoodIndex = i
+		}
+	}
+	firstIndexValue := nums[0]
+	return firstIndexValue >= nearestGoodIndex
+}
+
+// func main() {
+// 	var num int
+// 	for {
+// 		fmt.Scanln(&num)
+// 		if num == -1 {
+// 			break
+// 		}
+
+// 		var nums = make([]int, num)
+
+// 		for i, _ := range nums {
+// 			fmt.Scanf("%d", &nums[i])
+// 		}
+// 		fmt.Println("output : ", canJump(nums))
+// 	}
+// }
+
+// Test cases
+
+// [2,3,1,1,4] = true
+// [3,2,1,0,4] = false
+// [1] = true
+// [2,0] = true
