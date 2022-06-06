7100c6f7d6f32b3ab682ed782e9f37b52d69dd34
Mir Mursalin Ankur <mir.ankur.ruet13@gmail.com>
2022-06-06 09:10:25 +0600

    Time: 248 ms (33.10%), Space: 52.8 MB (7.92%) - LeetHub

---
+++ 1763-longest-nice-substring/1763-longest-nice-substring.js
@@ -0,0 +1,107 @@
+"use strict";
+/*
+ * Ankur Mursalin
+ *
+ * https://encryptioner.github.io/
+ *
+ * Created on Mon Jun 06 2022
+ */
+const isUpperCase = (s) => /^[A-Z]*$/.test(s);
+const getAlphabet = () => {
+    const alphabet = [];
+    for (let i = 0; i < 26; i++) {
+        alphabet.push({
+            hasLowercase: false,
+            hasUppercase: false,
+        });
+    }
+    return alphabet;
+};
+const getAllSubstrings = (str, size) => {
+    const result = [];
+    for (let i = 0; i < str.length; i++) {
+        for (let j = str.length; j - i >= size; j--) {
+            result.push(str.slice(i, j));
+        }
+    }
+    return result;
+};
+const checkIfNiceString = (str) => {
+    const alphabet = getAlphabet();
+    for (let i = 0; i < str.length; i++) {
+        const index = str[i].toLowerCase().charCodeAt(0) - 97;
+        const key = isUpperCase(str[i]) ? 'hasUppercase' : 'hasLowercase';
+        alphabet[index][key] = true;
+    }
+    for (let i = 0; i < 26; i++) {
+        const isPresent = alphabet[i].hasLowercase || alphabet[i].hasUppercase;
+        const bothCasePresent = alphabet[i].hasLowercase && alphabet[i].hasUppercase;
+        if (isPresent && !bothCasePresent) {
+            return false;
+        }
+    }
+    return true;
+};
+var longestNiceSubstring = function(s) {
+    let str = s;
+    let longestNiceSubString = '';
+    if (str.length === 1) {
+        return longestNiceSubString;
+    }
+    if (str.length === 2) {
+        const index0 = str[0].toLowerCase().charCodeAt(0) - 97;
+        const index1 = str[1].toLowerCase().charCodeAt(0) - 97;
+        if (index0 === index1) {
+            return str;
+        }
+        return longestNiceSubString;
+    }
+    const alphabet = getAlphabet();
+    for (let i = 0; i < str.length; i++) {
+        const index = str[i].toLowerCase().charCodeAt(0) - 97;
+        const key = isUpperCase(str[i]) ? 'hasUppercase' : 'hasLowercase';
+        alphabet[index][key] = true;
+    }
+    for (let i = 0; i < str.length; i++) {
+        const index = str[i].toLowerCase().charCodeAt(0) - 97;
+        if (!alphabet[index].hasLowercase || !alphabet[index].hasUppercase) {
+            str = str.substring(0, i) + ' ' + str.substring(i + 1);
+        }
+    }
+    const splittedStrings = str.split(' ');
+    const niceStrings = [];
+    for (let i = 0; i < splittedStrings.length; i++) {
+        if (splittedStrings[i].length < 2) {
+            continue;
+        }
+        let subStrings = [splittedStrings[i]];
+        if (splittedStrings[i].length > 2) {
+            subStrings = getAllSubstrings(splittedStrings[i], 2);
+        }
+        for (let j = 0; j < subStrings.length; j++) {
+            const isNice = checkIfNiceString(subStrings[j]);
+            if (isNice) {
+                niceStrings.push(subStrings[j]);
+            }
+        }
+    }
+    for (let i = 0; i < niceStrings.length; i++) {
+        if (niceStrings[i].length > longestNiceSubString.length) {
+            longestNiceSubString = niceStrings[i];
+        }
+    }
+    // console.log(splittedStrings);
+    // console.log(niceStrings);
+    // console.log('longestNiceSubString = ', longestNiceSubString);
+    return longestNiceSubString;
+};
+// const inputs = ['abABB', 'YazaAay', 'Bb', 'c'];
+// const outputs = ['abABB', 'aAa', 'Bb', ''];
+// for (let i = 0; i < inputs.length; i++) {
+//     if (longestNiceSubstring(inputs[i]) === outputs[i]) {
+//         console.log('PASSED');
+//         continue;
+//     }
+//     console.log('FAILED');
+// }
+//# sourceMappingURL=1763.js.map
