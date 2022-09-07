eb7c702ac8733ec334d5bb3bd606fd09011e673a
Mir Mursalin Ankur <mir.ankur.ruet13@gmail.com>
2022-09-07 09:07:46 +0600

    Time: 77 ms (96.30%), Space: 48.7 MB (29.63%) - LeetHub

---
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function tree2str(root: TreeNode | null): string {
+  if (root === null) {
+    return '';
+  }
+  if (root.left === null && root.right === null) {
+    return `${root.val}`;
+  }
+  if (root.right == null) {
+    return `${root.val}(${tree2str(root.left)})`;
+  }
+  return `${root.val}(${tree2str(root.left)})(${tree2str(root.right)})`;
+}
