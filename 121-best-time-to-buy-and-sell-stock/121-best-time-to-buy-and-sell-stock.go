dba4b725edd69b6fe10298927af9f8f45e0cedcb
Mir Mursalin Ankur <mir.ankur.ruet13@gmail.com>
2022-09-10 15:43:35 +0600

    Time: 270 ms (16.36%), Space: 8.5 MB (78.46%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.go
@@ -0,0 +1,32 @@
+/*
+ * Ankur Mursalin
+ *
+ * https://encryptioner.github.io/
+ *
+ * Created on Sat Sep 10 2022
+ */
+
+// REFERENCE: https://leetcode.com/problems/best-time-to-buy-and-sell-stock
+
+package main
+
+func maxProfit(prices []int) int {
+	maxProfitVal := 0
+	lowestBuyVal := prices[0]
+
+	for _, price := range prices {
+		profit := price - lowestBuyVal
+		if profit > maxProfitVal {
+			maxProfitVal = profit
+		}
+		if price < lowestBuyVal {
+      lowestBuyVal = price;
+    }
+	}
+	return maxProfitVal
+}
+
+// func main() {
+// 	arr := [5]int{1, 5, 3, 6, 4}
+// 	maxProfit(arr[:])
+// }
